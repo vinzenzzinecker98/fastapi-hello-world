bf37e85e8cbfb8d7ca928f60950da176b953a8ed
Vinzenz Zinecker <vinzenz.zinecker@exxeta.com>
2024-11-21 14:46:59 +0100

    use api key

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI
 
+from fastapi import FastAPI, HTTPException
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+our_key = os.environ["OUR_API_KEY"]
 
 app = FastAPI(
               swagger_ui_parameters={"tryItOutEnabled": True},
               title="Virtual Support Bot",)#
 
 @app.post("/hello_world")
-def hello_world(req:str):
-    return f"hello world, got your {req}"
+def hello_world(req:str, key:str):
+    if key == our_key:
+        return f"hello world, got your {req}"
+    else:
+        raise HTTPException(status_code=401, detail="No key provided")
